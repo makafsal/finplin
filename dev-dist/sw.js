76eaf87dec7a7d4223b4bed56bc99363547f6fae
Afsal K <makafsal@gmail.com>
2025-04-09 21:39:48 +0530

    overfloe

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.q0pf0rhspi"
+    "revision": "0.ksm1ivo96t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
